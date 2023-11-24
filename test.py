9cb98a72b28c417a73e303b9bd3eaafc5d36645a
black_chick <dauspenskiy@edu.hse.ru>
2023-11-25 01:03:28 +0300

    Removed redundant sample after debug

--- test.py
+++ test.py
@@ -45,8 +45,7 @@ def main(config, out_file):
     sentences = [
         'A defibrillator is a device that gives a high energy electric shock to the heart of someone who is in cardiac arrest',
         'Massachusetts Institute of Technology may be best known for its math, science and engineering education',
-        'Wasserstein distance or Kantorovich Rubinstein metric is a distance function defined between probability distributions on a given metric space',
-        'I love my mom and dads'
+        'Wasserstein distance or Kantorovich Rubinstein metric is a distance function defined between probability distributions on a given metric space'
     ]
 
     param_mapping = {
